b383b84a1776c2d55b7b5ec114aac90c9da13202
Huseyn Ismayilov <131815459+Huseyn-Ismayilov@users.noreply.github.com>
2024-06-07 00:20:41 +0400

    build error fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import ScrollToTop from '../utils/ScrollToTop'
 import Home from '../pages/Home'
 import About from '../pages/About'
-import Porfolio from '../pages/Porfolio'
+import Portfolio from '../pages/Portfolio'
 import Blog from '../pages/Blog'
 import BlogDetails from '../pages/BlogDetails'
 import { Layout } from '../layout/Layout'
@@ -15,7 +15,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/portfolio' element={<Porfolio />} />
+          <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/blog' element={<Blog />} />
           <Route path='/blog/:slug' element={<BlogDetails />} />
         </Route>
